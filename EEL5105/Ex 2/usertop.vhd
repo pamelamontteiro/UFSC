feab9d40be2d9591986af0f51f60dc8a46c338d1
Pamela Monteiro <pamela.csmonteiro@gmail.com>
2024-06-24 22:25:52 -0300

    atualização

--- EEL5105/Ex 2/usertop.vhd
+++ EEL5105/Ex 2/usertop.vhd
@@ -1,21 +1,59 @@
-library IEEE; 
-use IEEE.Std_Logic_1164.all; 
+library IEEE;
+use IEEE.Std_Logic_1164.all;
+use IEEE.std_logic_signed.all; 
 
 entity usertop is
-port(
-	CLOCK_50:in std_logic;
-	CLK_500Hz:in std_logic;
-	KEY:in std_logic_vector(3 downto 0);
-	SW:in std_logic_vector(17 downto 0);
-	LEDR:out std_logic_vector(17 downto 0);
-	HEX0,HEX1,HEX2,HEX3,HEX4,HEX5,HEX6,HEX7: out std_logic_vector(6 downto 0));
+port(SW:in std_logic_vector(17 downto 0);
+     HEX0,HEX1,HEX2,HEX3,HEX4,HEX5,HEX6: out std_logic_vector(6 downto 0));
 end usertop;
 
-architecture circuito of usertop is
+architecture circuito_exe2 of usertop is
+signal A, F, F1, F2, F3, F4: std_logic_vector(3 downto 0); 
+signal C: std_logic_vector(1 downto 0); 
 
-begin    
-    LEDR(3) <= ((not SW(3)) and (not SW(2)) and  SW(1) and SW(0)) or (SW(2) and SW(1) and (not SW(0))) or (SW(3) and (not SW(1)) and SW(0)) or (SW(3) and SW(2));
-    LEDR(2) <= (SW(3) and SW(2)) or (not SW(0));
-    LEDR(1) <= (SW(2)and SW(1)) or (SW(3));
-    LEDR(0) <= ((not SW(3)) and (not SW(2)) and  SW(1)) or ((SW(1)and SW(0)) or (SW(2)and SW(0)));
-end circuito; 
+component mux4_1 is
+port (F1,F2,F3,F4: in std_logic_vector(3 downto 0);
+sel: in std_logic_vector(1 downto 0);
+F: out std_logic_vector(3 downto 0)
+);
+end component;
+
+component mux2_1 is
+port (
+F1: in std_logic_vector(6 downto 0);
+F2: in std_logic_vector(6 downto 0);
+sel: in std_logic;
+F: out std_logic_vector(6 downto 0)
+);
+end component;
+
+component decod_C2 is
+port (G: in std_logic_vector(3 downto 0);
+F: out std_logic_vector(6 downto 0)
+);
+end component;
+
+component  decod is
+port (G: in std_logic_vector(1 downto 0);
+F: out std_logic_vector(6 downto 0)
+);
+end component;
+
+begin
+M1: mux4_1 port map (F1,F2,F3,F4,C,F);
+M2: mux2_1 port map ("1111111","0111111",A(3),HEX6);
+M3: mux2_1 port map ("0001111","0111111",C(1),HEX4);
+M4: mux2_1 port map ("1111111","0111111",F(3),HEX1);
+D3: decod_C2 port map (A,HEX5);
+D2: decod_C2 port map (F,HEX0);
+D1: decod port map (C,HEX3);
+A <= SW(3 downto 0);
+C <= SW(9 downto 8);
+
+HEX2 <= "0110111";
+F1 <= A + 4;
+F2 <= A + 5;
+F3 <= A - 2;
+F4 <= A - 1;
+
+end circuito_exe2;
